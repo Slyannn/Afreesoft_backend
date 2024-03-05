5d2efaf30a6c75da9fb611c0ca0db1e0b44a0f66
koyeguibaissi <koyeguibaissi@et.esiea.fr>
2024-03-05 13:43:12 +0100

    add service send email

--- src/Service/SendMailService.php
+++ src/Service/SendMailService.php
@@ -1,8 +1,39 @@
 <?php
+declare(strict_types=1);
 
 namespace App\Service;
 
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+use Symfony\Component\Mailer\MailerInterface;
+
 class SendMailService
 {
+    private MailerInterface $mailer;
+    
+    public function __construct(MailerInterface $mailer)
+    {
+        $this->mailer = $mailer;
+    }
 
+    public function send(
+        string $from,
+        string $to,
+        string $subject,
+        string $template,
+        array $context
+    ):void
+    {
+        //creer le mail
+        $email = (new TemplatedEmail())
+            ->from($from)
+            ->to($to)
+            ->subject($subject)
+            ->htmlTemplate("email/$template.html.twig")
+            ->context($context);
+
+        //envoie le mail
+        $this->mailer->send($email);
+
+
+    }
 }
